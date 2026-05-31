c734fdc362019d745916766780f93c7d4ad0c941
Dmorales13110 <dmorales@insightnexus.io>
2026-05-31 19:30:51 -0400

    fix: added missing types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+// types/index.ts
 export interface FormValues {
   // Personal Information
   fullName: string;
@@ -18,3 +19,17 @@ export interface FormValues {
 export type FlavorValue = 'green-apple' | 'blue-razz' | 'pink-vanilla' | 'pineapple' | 'cherry' | 'grape';
 export type EventTypeValue = 'wedding' | 'birthday' | 'corporate' | 'baby-shower' | 'graduation' | 'festival' | 'other';
 export type DurationValue = '1' | '2' | '3' | '4' | '5';
+export interface Flavor {
+  label: string;
+  value: FlavorValue;
+}
+
+export interface EventType {
+  value: EventTypeValue;
+  label: string;
+}
+
+export interface DurationOption {
+  value: DurationValue;
+  label: string;
+}
